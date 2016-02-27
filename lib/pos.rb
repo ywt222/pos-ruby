83ced6f44d6b09c78a72d03ccb9f1a432028bcbf
Wenting Yan <1055098134@qq.com>
2016-02-27 18:50:46 +0800

    finish task

--- lib/pos.rb
+++ lib/pos.rb
@@ -3,8 +3,10 @@ require 'json'
 require 'pp'
 
 
-# task1:count the items in goods-example.json
+
 class CountGoods
+
+# task1:count the items in goods-example.json
   def count_item
     goods = File.read('../spec/fixture/goods-example.json')
     goods_example = JSON.parse(goods)
@@ -26,7 +28,7 @@ class CountGoods
       end
     end
     @goods_buy
-  end
+  end   #   def count_item   end
 
 
 # task2:load all items from all-items.json
@@ -35,7 +37,7 @@ class CountGoods
     all_goods = JSON.parse(all)
     goods_buy_array = Array.new
     goods_buy_array = @goods_buy.to_a
-    goods_detail = Array.new
+    @goods_detail = Array.new
     @all_goods_ini = Array.new
 
     all_goods.each do |item|
@@ -61,16 +63,88 @@ class CountGoods
 
     @all_goods_ini.each do |item|
       if item["count"] != 0
-        goods_detail << item
+        @goods_detail << item
       end
     end
 
-    goods_detail
+    @goods_detail
 
   end # def item_detail   end
+
+
+  # task3: load the promotions barcodes
+  def promotion_item
+    promotions = File.read('../spec/fixture/promotions.json')
+    @pro_item = JSON.parse(promotions)
+    @pro_item[0]["barcodes"]
+
+  end   # def promotion_item   end
+
+
+  # task4: get the promotion of the goods we buy
+  def youhui_goods
+    @goods_detail_new = Array.new
+    @goods_detail.each do |item|
+      @goods_detail_new << item.merge("new_count"=>"#{item["count"]}".to_i)
+    end
+    @pro_item[0]["barcodes"].each do |i|
+      @goods_detail_new.each do |j|
+        if i == j["barcode"]
+          j["new_count"] = j["count"]-j["count"]/3
+        end
+      end
+    end
+    @goods = Array.new
+    @goods_detail_new.each do |item|
+      total_price = item["price"]*item["new_count"]
+      discount = item["price"]*(item["count"]-item["new_count"])
+      @goods << item.merge("total_price"=>"#{total_price}".to_f,"discount"=>"#{discount}".to_f)
+    end
+    @goods
+
+  end   # def youhui_goods   end
+
+
+# task5: get the total cost and the total discount
+  def total_goods
+    @total = Hash.new
+    @total = {"total_cost"=>0,"total_discount"=>0}
+    @goods.each do |item|
+      @total["total_cost"] += item["total_price"]
+      @total["total_discount"] += item["discount"]
+    end
+    @total
+
+  end #  def total_goods    end
+
+
+# task6: print the shopping list
+  def shopping_list
+    p "***<没钱赚商店>购物清单***"
+    @goods.each do |item|
+      p "名称："+item["name"]+"，数量："+item["count"].to_s+item["unit"]+"，单价："+item["price"].to_s+"(元)，小计："+item["total_price"].to_s+"(元)"
+    end
+    p "----------------------"
+    p "挥泪赠送商品："
+    @goods.each do |item|
+      if item["discount"] != 0
+        p "名称："+item["name"]+"，数量："+(item["count"]-item["new_count"]).to_s+item["unit"]
+      end
+    end
+    p "----------------------"
+    p "总计："+@total["total_cost"].to_s+"(元)"
+    p "节省："+@total["total_discount"].to_s+"(元)"
+    p "**********************"
+
+  end   #   def shopping_list   end
+
 
 end # class    end
 
 #a = CountGoods.new
 #a.count_item
 #a.item_detail
+#a.promotion_item
+#a.youhui_goods
+#a.total_goods
+#a.shopping_list
