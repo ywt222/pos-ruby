7dbd4404c874c2cd2b122a77eaf8ca2e8660c5f4
Wenting Yan <1055098134@qq.com>
2016-03-08 22:51:19 +0800

    modify some format questions and use class method instead of instance method

--- lib/pos.rb
+++ lib/pos.rb
@@ -1,143 +1,146 @@
 require 'rubygems'
 require 'json'
-require 'pp'
-
-
 
 class CountGoods
-
+  class << self   # def class method,also can be def self.count_item(goods) which is same to the other methods
 # task1:count the items in goods-example.json
-  def count_item(goods)
 
-    goods_buy = Hash.new
-    goods.each do |item|
+    def count_item(goods)
 
-      barcode = item.split('-')[0]
-      num = item.split('-')[1]
-      value = num.nil? ? 1: num.to_i
+      goods_buy = Hash.new
+      goods.each do |item|
 
-      if goods_buy[barcode].nil?
-        goods_buy[barcode] = value
-      else
-        goods_buy[barcode] += value
+        barcode = item.split('-')[0]
+        num = item.split('-')[1]
+        value = num.nil? ? 1: num.to_i
+
+        if goods_buy[barcode].nil?
+          goods_buy[barcode] = value
+        else
+          goods_buy[barcode] += value
+        end
       end
-    end
-    goods_buy
-  end   #   def count_item   end
+      goods_buy
+    end   #   def count_item   end
 
 
 # task2:load all items from all-items.json
-  def item_detail(all_goods,goods_buy)
-    goods_buy_array = Array.new
-    goods_buy_array = goods_buy.to_a
-    goods_detail = Array.new
-    all_goods_ini = Array.new
+    BARCODE = 0
+    COUNT = 1
+    def item_detail(all_goods,goods_buy)
+      #goods_buy_array = Array.new
+      goods_buy_array = goods_buy.to_a
+      goods_detail = Array.new
+      all_goods_ini = Array.new
 
-    all_goods.each do |item|
-      all_goods_ini << item.merge("count"=>0)
-    end
+      all_goods.each do |item|
+        all_goods_ini << item.merge('count'=>0)
+      end
 
 # 46-52行也可为54-60行
-    all_goods_ini.each do |i|
-      goods_buy_array.each do |j|
-        if i["barcode"] == j[0]
-          i["count"] += j[1]
+      all_goods_ini.each do |i|
+        goods_buy_array.each do |j|
+          if i['barcode'] == j[BARCODE]
+            i['count'] = j[COUNT]
+          end
         end
       end
-    end
 
-    #@all_goods_ini.each do |item|
-    #  for j in 0..goods_buy_array.size-1
-    #    if item["barcode"] == goods_buy_array[j][0]
-    #      item["count"] += goods_buy_array[j][1]
-    #    end
-    #  end
-    #end
+      #@all_goods_ini.each do |item|
+      #  for j in 0..goods_buy_array.size-1
+      #    if item["barcode"] == goods_buy_array[j][0]
+      #      item["count"] += goods_buy_array[j][1]
+      #    end
+      #  end
+      #end
 
-    all_goods_ini.each do |item|
-      if item["count"] != 0
-        goods_detail << item
+      all_goods_ini.each do |item|
+        if item['count'] != 0
+          goods_detail << item
+        end
       end
-    end
 
-    goods_detail
+      goods_detail
 
-  end # def item_detail   end
+    end # def item_detail   end
 
 
   # task3: load the promotions barcodes
-  def promotion_item(promotions)
-    prom_barcode = promotions[0]["barcodes"]
-  end   # def promotion_item   end
+    def promotion_item(promotions)
+      promotions[0]['barcodes']
+    end   # def promotion_item   end
 
 
-  # task4: get the promotion of the goods we buy
-  def youhui_goods(goods_detail,prom_barcode)
-    goods_detail_new = Array.new
-    goods_detail.each do |item|
-      goods_detail_new << item.merge("new_count"=>"#{item["count"]}".to_i)
-    end
-    prom_barcode.each do |i|
-      goods_detail_new.each do |j|
-        if i == j["barcode"]
-          j["new_count"] = j["count"]-j["count"]/3
+    # task4: get the promotion of the goods we buy
+    def promotion_goods(goods_detail,prom_barcode)
+      goods_detail_new = Array.new
+      goods_detail.each do |item|
+        goods_detail_new << item.merge('new_count'=>"#{item['count']}".to_i)
+      end
+      prom_barcode.each do |i|
+        goods_detail_new.each do |j|
+          if i == j['barcode']
+            j['new_count'] = j['count']-j['count']/3
+          end
         end
       end
-    end
-    discount_goods = Array.new
-    goods_detail_new.each do |item|
-      total_price = item["price"]*item["new_count"]
-      discount = item["price"]*(item["count"]-item["new_count"])
-      discount_goods << item.merge("total_price"=>"#{total_price}".to_f,"discount"=>"#{discount}".to_f)
-    end
-    discount_goods
+      discount_goods = Array.new
+      goods_detail_new.each do |item|
+        total_price = item['price']*item['new_count']
+        discount = item['price']*(item['count']-item['new_count'])
+        discount_goods << item.merge('total_price'=>"#{total_price}".to_f,'discount'=>"#{discount}".to_f)
+      end
+      discount_goods
 
-  end   # def youhui_goods   end
+    end   # def promotion_goods   end
 
 
-# task5: get the total cost and the total discount
-  def total_goods(discount_goods)
-    total_info = Hash.new
-    total_info = {"total_cost"=>0,"total_discount"=>0}
-    discount_goods.each do |item|
-      total_info["total_cost"] += item["total_price"]
-      total_info["total_discount"] += item["discount"]
-    end
-    total_info
+  # task5: get the total cost and the total discount
+    def total_goods(discount_goods)
+      #total_info = Hash.new
+      total_info = {'total_cost'=>0,'total_discount'=>0}
+      discount_goods.each do |item|
+        total_info['total_cost'] += item['total_price']
+        total_info['total_discount'] += item['discount']
+      end
+      total_info
 
-  end #  def total_goods    end
+    end #  def total_goods    end
 
 
 # task6: print the shopping list
-  def shopping_list(discount_goods,total_info)
-    p "***<没钱赚商店>购物清单***"
-    discount_goods.each do |item|
-      p "名称："+item["name"]+"，数量："+item["count"].to_s+item["unit"]+"，单价："+item["price"].to_s+"(元)，小计："+item["total_price"].to_s+"(元)"
-    end
-    p "----------------------"
-    p "挥泪赠送商品："
-    discount_goods.each do |item|
-      if item["discount"] != 0
-        p "名称："+item["name"]+"，数量："+(item["count"]-item["new_count"]).to_s+item["unit"]
+    def shopping_list(discount_goods,total_info)
+      p '***<没钱赚商店>购物清单***'
+      discount_goods.each do |item|
+        #p "名称："+item['name']+"，数量："+item['count'].to_s+item['unit']+"，单价："+item['price'].to_s+"(元)，小计："+item['total_price'].to_s+"(元)"
+        p "名称：#{item['name']}，数量：#{item['count']}#{item['unit']}，单价：#{item['price']}(元)，小计：#{item['total_price']}(元)"
       end
-    end
-    p "----------------------"
-    p "总计："+total_info["total_cost"].to_s+"(元)"
-    p "节省："+total_info["total_discount"].to_s+"(元)"
-    p "**********************"
+      p '----------------------'
+      p '挥泪赠送商品：'
+      discount_goods.each do |item|
+        if item['discount'] != 0
+          #p "名称："+item['name']+"，数量："+(item['count']-item['new_count']).to_s+item['unit']
+          p "名称：#{item['name']}，数量：#{item['count']-item['new_count']}#{item['unit']}"
+        end
+      end
+      p '----------------------'
+      #p "总计："+total_info['total_cost'].to_s+"(元)"
+      p "总计：#{total_info['total_cost']}(元)"
+      #p "节省："+total_info['total_discount'].to_s+"(元)"
+      p "节省：#{total_info['total_discount']}(元)"
+      p '**********************'
 
-  end   #   def shopping_list   end
+    end   #   def shopping_list   end
 
-
+  end   #   class << self end
 end # class    end
 
-#pos_goods = CountGoods.new
 #goods = JSON.parse(File.read('../spec/fixture/goods-example.json'))
 #all_goods = JSON.parse(File.read('../spec/fixture/all-items.json'))
-#goods_buy = pos_goods.count_item(goods)
-#goods_detail = pos_goods.item_detail(all_goods,goods_buy)
+#goods_buy = CountGoods.count_item(goods)
+#goods_detail = CountGoods.item_detail(all_goods,goods_buy)
 #promotions = JSON.parse(File.read('../spec/fixture/promotions.json'))
-#prom_barcode = pos_goods.promotion_item(promotions)
-#discount_goods = pos_goods.youhui_goods(goods_detail,prom_barcode)
-#total_info = pos_goods.total_goods(discount_goods)
-#pos_goods.shopping_list(discount_goods,total_info)
+#prom_barcode = CountGoods.promotion_item(promotions)
+#discount_goods = CountGoods.promotion_goods(goods_detail,prom_barcode)
+#total_info = CountGoods.total_goods(discount_goods)
+#CountGoods.shopping_list(discount_goods,total_info)
--- spec/pos_spec.rb
+++ spec/pos_spec.rb
@@ -1,101 +1,104 @@
-require '../lib/pos'
+require_relative '../lib/pos'
 #require './goods-example.json'
 
 describe CountGoods do
   #  task1_spec
-  describe "#count" do
-    it "returns the barcode and count of the goods you buy" do
-      pos_goods = CountGoods.new
+  describe '.count_item' do
+    it 'returns the barcode and count of the goods you buy' do
       goods = JSON.parse(File.read('../spec/fixture/goods-example.json'))
-      goods_buy = pos_goods.count_item(goods)
-      expect({"ITEM000001"=>5, "ITEM000003"=>2, "ITEM000005"=>3}).to eq(goods_buy)
+      goods_buy = CountGoods.count_item(goods)
+      expect({'ITEM000001'=>5, 'ITEM000003'=>2, 'ITEM000005'=>3}).to eq(goods_buy)
     end
-  end   #   "#count"   end
+  end   #   '.count_item'   end
 
   #  task2_spec
-  describe "#detail" do
-    it "returns the detail information of the goods you buy" do
-      pos_goods = CountGoods.new
+  describe '.item_detail' do
+    it 'returns the detail information of the goods you buy' do
       goods = JSON.parse(File.read('../spec/fixture/goods-example.json'))
       all_goods = JSON.parse(File.read('../spec/fixture/all-items.json'))
-      goods_buy = pos_goods.count_item(goods)
-      goods_detail = pos_goods.item_detail(all_goods,goods_buy)
-      expect([{"barcode"=>"ITEM000001", "name"=>"雪碧", "unit"=>"瓶", "price"=>3.0, "count"=>5},
-         {"barcode"=>"ITEM000003", "name"=>"荔枝", "unit"=>"斤", "price"=>15.0, "count"=>2},
-         {"barcode"=>"ITEM000005", "name"=>"方便面", "unit"=>"袋", "price"=>4.5, "count"=>3}]).to eq(goods_detail)
+      goods_buy = {'ITEM000001'=>5, 'ITEM000003'=>2, 'ITEM000005'=>3}
+      goods_detail = CountGoods.item_detail(all_goods,goods_buy)
+      expect([{'barcode'=>'ITEM000001', 'name'=>'雪碧', 'unit'=>'瓶', 'price'=>3.0, 'count'=>5},
+         {'barcode'=>'ITEM000003', 'name'=>'荔枝', 'unit'=>'斤', 'price'=>15.0, 'count'=>2},
+         {'barcode'=>'ITEM000005', 'name'=>'方便面', 'unit'=>'袋', 'price'=>4.5, 'count'=>3}]).to eq(goods_detail)
     end
-  end   #   "#detail"   end
+  end   #   '.item_detail'   end
 
   #   task3_spec
-  describe "promotions" do
-    it "returns the barcodes of the promotions item" do
-    pos_goods = CountGoods.new
+  describe '.promotion_item' do
+    it 'returns the barcodes of the promotions item' do
     promotions = JSON.parse(File.read('../spec/fixture/promotions.json'))
-    prom_barcode = pos_goods.promotion_item(promotions)
-    expect(["ITEM000000", "ITEM000001", "ITEM000005"]).to eq(prom_barcode)
+    prom_barcode = CountGoods.promotion_item(promotions)
+    expect(['ITEM000000', 'ITEM000001', 'ITEM000005']).to eq(prom_barcode)
     end
-  end   #   "promotions"   end
+  end   #   '.promotion_item'   end
 
   #   task4_spec
-  describe "discount" do
-    it "get the total price you need to pay for every good with its discount" do
-      pos_goods = CountGoods.new
+  describe '.promotion_goods' do
+    it 'get the total price you need to pay for every good with its discount' do
       goods = JSON.parse(File.read('../spec/fixture/goods-example.json'))
       all_goods = JSON.parse(File.read('../spec/fixture/all-items.json'))
-      goods_buy = pos_goods.count_item(goods)
-      goods_detail = pos_goods.item_detail(all_goods,goods_buy)
+      goods_buy = {'ITEM000001'=>5, 'ITEM000003'=>2, 'ITEM000005'=>3}
+      goods_detail = [{'barcode'=>'ITEM000001', 'name'=>'雪碧', 'unit'=>'瓶', 'price'=>3.0, 'count'=>5},
+         {'barcode'=>'ITEM000003', 'name'=>'荔枝', 'unit'=>'斤', 'price'=>15.0, 'count'=>2},
+         {'barcode'=>'ITEM000005', 'name'=>'方便面', 'unit'=>'袋', 'price'=>4.5, 'count'=>3}]
 
-      promotions = JSON.parse(File.read('../spec/fixture/promotions.json'))
-      prom_barcode = pos_goods.promotion_item(promotions)
+      prom_barcode = ['ITEM000000', 'ITEM000001', 'ITEM000005']
 
-      discount_goods = pos_goods.youhui_goods(goods_detail,prom_barcode)
-      expect([{"barcode"=>"ITEM000001", "name"=>"雪碧", "unit"=>"瓶", "price"=>3.0, "count"=>5, "new_count"=>4, "total_price"=>12.0, "discount"=>3.0},
-         {"barcode"=>"ITEM000003", "name"=>"荔枝", "unit"=>"斤", "price"=>15.0, "count"=>2, "new_count"=>2, "total_price"=>30.0, "discount"=>0.0},
-          {"barcode"=>"ITEM000005", "name"=>"方便面", "unit"=>"袋", "price"=>4.5, "count"=>3, "new_count"=>2, "total_price"=>9.0, "discount"=>4.5}])
+      discount_goods = CountGoods.promotion_goods(goods_detail,prom_barcode)
+      expect([{'barcode'=>'ITEM000001', 'name'=>'雪碧', 'unit'=>'瓶', 'price'=>3.0, 'count'=>5, 'new_count'=>4, 'total_price'=>12.0, 'discount'=>3.0},
+         {'barcode'=>'ITEM000003', 'name'=>'荔枝', 'unit'=>'斤', 'price'=>15.0, 'count'=>2, 'new_count'=>2, 'total_price'=>30.0, 'discount'=>0.0},
+          {'barcode'=>'ITEM000005', 'name'=>'方便面', 'unit'=>'袋', 'price'=>4.5, 'count'=>3, 'new_count'=>2, 'total_price'=>9.0, 'discount'=>4.5}])
           .to eq(discount_goods)
     end
-  end   #   "discount"   end
+  end   #   '.promotion_goods'   end
 
   #   task5_spec
-  describe "total" do
-    it "get the total cost and total discount for all the goods" do
-      pos_goods = CountGoods.new
+  describe '.total_goods' do
+    it 'get the total cost and total discount for all the goods' do
 
       goods = JSON.parse(File.read('../spec/fixture/goods-example.json'))
       all_goods = JSON.parse(File.read('../spec/fixture/all-items.json'))
-      goods_buy = pos_goods.count_item(goods)
-      goods_detail = pos_goods.item_detail(all_goods,goods_buy)
+      goods_buy = {'ITEM000001'=>5, 'ITEM000003'=>2, 'ITEM000005'=>3}
+      goods_detail = [{'barcode'=>'ITEM000001', 'name'=>'雪碧', 'unit'=>'瓶', 'price'=>3.0, 'count'=>5},
+         {'barcode'=>'ITEM000003', 'name'=>'荔枝', 'unit'=>'斤', 'price'=>15.0, 'count'=>2},
+         {'barcode'=>'ITEM000005', 'name'=>'方便面', 'unit'=>'袋', 'price'=>4.5, 'count'=>3}]
 
       promotions = JSON.parse(File.read('../spec/fixture/promotions.json'))
-      prom_barcode = pos_goods.promotion_item(promotions)
+      prom_barcode = ['ITEM000000', 'ITEM000001', 'ITEM000005']
 
-      discount_goods = pos_goods.youhui_goods(goods_detail,prom_barcode)
+      discount_goods = [{'barcode'=>'ITEM000001', 'name'=>'雪碧', 'unit'=>'瓶', 'price'=>3.0, 'count'=>5, 'new_count'=>4, 'total_price'=>12.0, 'discount'=>3.0},
+         {'barcode'=>'ITEM000003', 'name'=>'荔枝', 'unit'=>'斤', 'price'=>15.0, 'count'=>2, 'new_count'=>2, 'total_price'=>30.0, 'discount'=>0.0},
+          {'barcode'=>'ITEM000005', 'name'=>'方便面', 'unit'=>'袋', 'price'=>4.5, 'count'=>3, 'new_count'=>2, 'total_price'=>9.0, 'discount'=>4.5}]
 
-      total_info = pos_goods.total_goods(discount_goods)
-      expect({"total_cost"=>51.0, "total_discount"=>7.5}).to eq(total_info)
+      total_info = CountGoods.total_goods(discount_goods)
+      expect({'total_cost'=>51.0, 'total_discount'=>7.5}).to eq(total_info)
     end
-  end   #   "total"   end
+  end   #   '.total_goods'   end
 
 
   #   task6_spec
-  #describe "print" do
-  #  it "print the shopping list" do
-  #    pos_goods = CountGoods.new
+  #describe '.shopping_list' do
+  #  it 'print the shopping list' do
   #    goods = JSON.parse(File.read('../spec/fixture/goods-example.json'))
   #    all_goods = JSON.parse(File.read('../spec/fixture/all-items.json'))
-  #    goods_buy = pos_goods.count_item(goods)
-  #    goods_detail = pos_goods.item_detail(all_goods,goods_buy)
+  #    goods_buy = {'ITEM000001'=>5, 'ITEM000003'=>2, 'ITEM000005'=>3}
+  #    goods_detail = [{'barcode'=>'ITEM000001', 'name'=>'雪碧', 'unit'=>'瓶', 'price'=>3.0, 'count'=>5},
+  #       {'barcode'=>'ITEM000003', 'name'=>'荔枝', 'unit'=>'斤', 'price'=>15.0, 'count'=>2},
+  #       {'barcode'=>'ITEM000005', 'name'=>'方便面', 'unit'=>'袋', 'price'=>4.5, 'count'=>3}]
 
   #    promotions = JSON.parse(File.read('../spec/fixture/promotions.json'))
-  #    prom_barcode = pos_goods.promotion_item(promotions)
+  #    prom_barcode = ['ITEM000000', 'ITEM000001', 'ITEM000005']
 
-  #    discount_goods = pos_goods.youhui_goods(goods_detail,prom_barcode)
+  #    discount_goods = [{'barcode'=>'ITEM000001', 'name'=>'雪碧', 'unit'=>'瓶', 'price'=>3.0, 'count'=>5, 'new_count'=>4, 'total_price'=>12.0, 'discount'=>3.0},
+  #       {'barcode'=>'ITEM000003', 'name'=>'荔枝', 'unit'=>'斤', 'price'=>15.0, 'count'=>2, 'new_count'=>2, 'total_price'=>30.0, 'discount'=>0.0},
+  #        {'barcode'=>'ITEM000005', 'name'=>'方便面', 'unit'=>'袋', 'price'=>4.5, 'count'=>3, 'new_count'=>2, 'total_price'=>9.0, 'discount'=>4.5}]
 
-  #    total_info = pos_goods.total_goods(discount_goods)
-  #    goods_list = pos_goods.shopping_list(discount_goods,total_info)
-  #    expect("").to eq(goods_list)
+  #    total_info = {'total_cost'=>51.0, 'total_discount'=>7.5}
+  #    goods_list = CountGoods.shopping_list(discount_goods,total_info)
+  #    expect('').to eq(goods_list)
   #  end
-  #end   #   "print"   end
+  #end   #   '.shopping_list'   end
 
 
 
